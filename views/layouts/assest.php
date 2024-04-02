2a383921325b5007c3cd15cb71e958140ab9a5d9
tatoshik92 <tatoshik92@yandex.ru>
2024-04-02 09:42:13 +0300

    1

--- views/layouts/assest.php
+++ views/layouts/assest.php
@@ -1,23 +1,28 @@
 <?php
+
 use app\assets\AssetsAsset;
 use yii\bootstrap4\Html;
+
 AssetsAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->t->lang['code_doctype_html']?>" dir="<?=Yii::$app->t->lang['dir']?>">
+<html lang="<?= Yii::$app->t->lang['code_doctype_html'] ?>" dir="<?= Yii::$app->t->lang['dir'] ?>">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
-    
+
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body class="dark">
-<?php $this->beginBody() ?>
-    <?=$content?>
-<?php $this->endBody() ?>
+
+<body <?= (Yii::$app->t->curentLang == 'ar' ? 'dir="rtl"' : '') ?>>
+    <?php $this->beginBody() ?>
+    <?= $content ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
